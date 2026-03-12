75bb8c6895968cc254abb1d73faff317b634e877
Lambda <2206402315@qq.com>
2026-03-12 20:11:22 +0800

    Site updated: 2026-03-12 20:11:21

--- js/aplayer/music-control.js
+++ js/aplayer/music-control.js
@@ -1,1 +1,79 @@
-!function(){ap=null,Object.defineProperty(document.querySelector("meting-js"),"aplayer",{set:function(i){ap=i,ap.on("canplay",(function(){if(!isRecover){if(e.enableRecover&&null!=localStorage.getItem("musicIndex")){if(musicIndex=localStorage.getItem("musicIndex"),musicTime=localStorage.getItem("musicTime"),ap.list.index==musicIndex)return ap.seek(musicTime),ap.play(),localStorage.clear(),void(isRecover=!0);ap.list.switch(musicIndex)}switch(e.specificMode){case"index":ap.list.index!=e.specificIndex&&ap.list.switch(e.specificIndex);break;case"name":const i=ap.list.audios.findIndex((i=>i.name.includes(e.specificName)));-1!==i&&i!==ap.list.index&&ap.list.switch(i);break;case"random":const a=Math.floor(Math.random()*ap.list.audios.length);a!==ap.list.index&&ap.list.switch(a)}e.autoPlay&&!ap.audio.paused&&ap.play(),isRecover=!0}}))}}),isRecover=!1;const e={enableRecover:!0,autoPlay:!0,specificMode:"first",specificIndex:3,specificName:"晴天"};window.onbeforeunload=function(e){ap.audio.paused||(localStorage.setItem("musicIndex",ap.list.index),localStorage.setItem("musicTime",ap.audio.currentTime))}}();
+// (function() {
+//     ap = null
+//     Object.defineProperty(document.querySelector('meting-js'),"aplayer",{
+//         set: function(aplayer) {
+//             ap=aplayer
+//             ready();
+//         }
+//     });
+//     isRecover = false;
+//
+//     // 配置区
+//     const musicConfig = {
+//         enableRecover: true,      // 启用恢复功能
+//         autoPlay: true,           // 自动播放
+//         specificMode: "first",    // 模式：first|index|name|random
+//         specificIndex: 3,         // 当模式为index时有效
+//         specificName: "晴天"      // 当模式为name时有效
+//     };
+//
+//     function ready(){
+//         ap.on('canplay', function () {
+//             if(!isRecover){
+//                 // 恢复播放检查
+//                 if(musicConfig.enableRecover && localStorage.getItem("musicIndex") != null){
+//                     musicIndex = localStorage.getItem("musicIndex");
+//                     musicTime = localStorage.getItem("musicTime");
+//                     if(ap.list.index != musicIndex){
+//                         ap.list.switch(musicIndex);
+//                     }else{
+//                         ap.seek(musicTime);
+//                         ap.play();
+//                         localStorage.clear();
+//                         isRecover = true;
+//                         return;
+//                     }
+//                 }
+//
+//                 // 根据配置选择播放策略
+//                 switch(musicConfig.specificMode){
+//                     case "index":
+//                         if(ap.list.index != musicConfig.specificIndex){
+//                             ap.list.switch(musicConfig.specificIndex);
+//                         }
+//                         break;
+//                     case "name":
+//                         const targetIndex = ap.list.audios.findIndex(song =>
+//                             song.name.includes(musicConfig.specificName)
+//                         );
+//                         if(targetIndex !== -1 && targetIndex !== ap.list.index){
+//                             ap.list.switch(targetIndex);
+//                         }
+//                         break;
+//                     case "random":
+//                         const randomIndex = Math.floor(Math.random() * ap.list.audios.length);
+//                         if(randomIndex !== ap.list.index){
+//                             ap.list.switch(randomIndex);
+//                         }
+//                         break;
+//                     case "first":
+//                     default:
+//                         // 默认播放第一首
+//                         break;
+//                 }
+//
+//                 if(musicConfig.autoPlay && !ap.audio.paused){
+//                     ap.play();
+//                 }
+//                 isRecover = true;
+//             }
+//         });
+//     }
+//
+//     window.onbeforeunload = function(event) {
+//         if(!ap.audio.paused){
+//             localStorage.setItem("musicIndex", ap.list.index);
+//             localStorage.setItem("musicTime", ap.audio.currentTime);
+//         }
+//     };
+// })();
